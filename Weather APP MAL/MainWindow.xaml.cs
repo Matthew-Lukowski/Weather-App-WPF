0568a34614041ac8adb246adf35fcf4b8386ad42
Matthew-Lukowski <78889270+Matthew-Lukowski@users.noreply.github.com>
2021-09-11 21:46:12 -0500

    FirstSubmit

---
+++ Weather APP MAL/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Windows;
+using Newtonsoft.Json;
+using System.Net;
+using System.ComponentModel;
+
+namespace Weather_APP_MAL {
+
+    public partial class MainWindow : Window {
+
+        readonly WebClient client = new();
+        string content = "";
+
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            Close();
+        }
+
+        private void Button_Click_Search(object sender, RoutedEventArgs e) => RunSearch();
+
+
+        private void SearchTermTextBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e) {
+            if(e.Key == System.Windows.Input.Key.Enter)
+                RunSearch();
+        }
+
+
+        private void RunSearch() {
+            string weatherInfoString = string.Empty;
+
+            if (!string.IsNullOrWhiteSpace(SearchTermTextBox.Text)) {
+                try {
+                    content = client.DownloadString($"http://api.weatherapi.com/v1/current.json?key={Key.API_KEY}&q={SearchTermTextBox.Text}&aqi=no");
+                }
+                catch (Exception) {
+                    TextBoxWeather.Text = "Location not found";
+                    return;
+                }
+            } else {
+                TextBoxWeather.Text = "Error: Search is empty";
+                return;
+            }
+
+            LocationRoot loc = JsonConvert.DeserializeObject<LocationRoot>(content);
+
+            foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(loc.Location)) {
+                string name = descriptor.Name;
+                object value = descriptor.GetValue(loc.Location);
+                weatherInfoString += $"{name}: {value}\n";
+            }
+
+            weatherInfoString += "\n";
+
+            foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(loc.CurrentTemp)) {
+                string name = descriptor.Name;
+                object value = descriptor.GetValue(loc.CurrentTemp);
+                weatherInfoString += $"{name}: {value}\n";
+            }
+            weatherInfoString = weatherInfoString.Replace("_", " ");
+
+            TextBoxWeather.Text = weatherInfoString;
+        }
+
+        private void TopBarDrag(object sender, System.Windows.Input.MouseButtonEventArgs e) {
+            if (e.ChangedButton == System.Windows.Input.MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void Button_Minimize(object sender, RoutedEventArgs e) {
+            WindowState = WindowState.Minimized;
+        }
+    }
+}
---
+++ Weather APP MAL/WeatherRequestObject.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+namespace Weather_APP_MAL {
+    public class LocationRoot {
+        [JsonProperty("location")]
+        public Location Location { get; set; }
+        [JsonProperty("current")]
+        public CurrentTemp CurrentTemp { get; set; }
+    }
+
+    public class Location {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("region")]
+        public string Region { get; set; }
+        [JsonProperty("country")]
+        public string Country { get; set; }
+        [JsonProperty("tz_id")]
+        public string Timezone { get; set; }
+        [JsonProperty("localtime")]
+        public string Localtime { get; set; }
+    }
+
+    public class CurrentTemp {
+        [JsonProperty("last_updated")]
+        public string Last_Updated_Time { get; set; }
+        [JsonProperty("temp_c")]
+        public double Temp_C { get; set; }
+        [JsonProperty("temp_f")]
+        public double Temp_F { get; set; }
+        [JsonProperty("uv")]
+        public double UV_Index { get; set; }
+    }
+}
